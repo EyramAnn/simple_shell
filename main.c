704caa1f6bad24ba4f7968e4a4dad1ed9196500c
SelasiEwurama <ewuramaselasi@gmail.com>
2023-11-16 20:57:30 -0800

    tasks

--- main.c
+++ main.c
@@ -1,44 +1,24 @@
-#include "shell.h"
+#include "shellby.h"
 
 /**
- * main - entry point
- * @ac: arg count
- * @av: arg vector
+ * main - execution of the shellby shell
+ * @argc: number of input arguments
+ * @argv: input arguments array
  *
- * Return: 0 on success, 1 on error
+ * Return: 0 on success
  */
-int main(int ac, char **av)
+
+int main(int argc, char *argv[])
 {
-	info_t info[] = { INFO_INIT };
-	int fd = 2;
-
-	asm ("mov %1, %0\n\t"
-		"add $3, %0"
-		: "=r" (fd)
-		: "r" (fd));
-
-	if (ac == 2)
+	if (isatty(STDIN_FILENO) != 1 || argc > 1)
 	{
-		fd = open(av[1], O_RDONLY);
-		if (fd == -1)
-		{
-			if (errno == EACCES)
-				exit(126);
-			if (errno == ENOENT)
-			{
-				_eputs(av[0]);
-				_eputs(": 0: Can't open ");
-				_eputs(av[1]);
-				_eputchar('\n');
-				_eputchar(BUF_FLUSH);
-				exit(127);
-			}
-			return (EXIT_FAILURE);
-		}
-		info->readfd = fd;
+		non_inter_shellby(argc, argv);
 	}
-	populate_env_list(info);
-	read_history(info);
-	hsh(info, av);
-	return (EXIT_SUCCESS);
+	else
+	{
+		inter_shellby();
+	}
+
+	exit(0);
+	return (0);
 }
